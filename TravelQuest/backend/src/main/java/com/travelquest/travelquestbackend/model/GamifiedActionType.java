e231bac06f65954aafdd121387d03960b6d9c69e
Ana-Maria Stefan <ana.maria.stefan03@gmail.com>
2026-02-03 12:14:41 +0200

    acum  level-ul se recalculeaza si dupa claim mission

--- TravelQuest/backend/src/main/java/com/travelquest/travelquestbackend/model/GamifiedActionType.java
+++ TravelQuest/backend/src/main/java/com/travelquest/travelquestbackend/model/GamifiedActionType.java
@@ -4,5 +4,7 @@ public enum GamifiedActionType {
     OBJECTIVE_APPROVED,       // turist +50 (xp_reward)
     ITINERARY_JOINED,         // ghid +25
     SUBMISSION_VALIDATED,     // ghid +5 (approve/reject)
-    MANUAL_ADJUSTMENT         // pentru debug/admin, dacă ai nevoie
+    MANUAL_ADJUSTMENT,        // pentru debug/admin, dacă ai nevoie
+    MISSION_CLAIM             // pentru xp castigat in momentul claim mission
+
 }
